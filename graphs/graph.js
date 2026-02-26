25388aa34d04643e3a9bc3394ae18ac4d96700b6
arthurnader-plank <arthur.nader@joinplank.com>
2026-02-26 01:21:51 -0300

    Create graph.js

---
+++ graphs/graph.js
@@ -0,0 +1,18 @@
+class Graph {
+    constructor(directed = false) {
+        this.adj = {};
+        this.directed = directed;
+    }
+
+    addNode(node) {
+        // TODO
+    }
+
+    addEdge(u, v) {
+        // TODO
+    }
+
+    getNeighbors(node) {
+        // TODO
+    }
+}
