a4a7a8df44cca473a4c3bf05507e3af3e2770749
arthurnader-plank <arthur.nader@joinplank.com>
2026-02-26 01:20:34 -0300

    Create graph.py

---
+++ graphs/graph.py
@@ -0,0 +1,13 @@
+class Graph:
+    def __init__(self, directed=False):
+        self.adj = {}
+        self.directed = directed
+
+    def add_node(self, node):
+        pass
+
+    def add_edge(self, u, v):
+        pass
+
+    def get_neighbors(self, node):
+        pass
