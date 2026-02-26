c899d62ac237961987573491acbaa4f3289b3579
arthurnader-plank <arthur.nader@joinplank.com>
2026-02-26 01:30:08 -0300

    Create graph.java

---
+++ graphs/graph.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Graph {
+    private Map<String, List<String>> adj;
+    private boolean directed;
+
+    public Graph(boolean directed) {
+        this.adj = new HashMap<>();
+        this.directed = directed;
+    }
+
+    public void addNode(String node) {
+        // TODO
+    }
+
+    public void addEdge(String u, String v) {
+        // TODO
+    }
+
+    public List<String> getNeighbors(String node) {
+        // TODO
+        return null;
+    }
+}
